33ace3ff02c9e25acb87a186cf2159ea5fe26691
Alexa Schreffler <alexaschreffler@alexas-mac-mini.lan>
2024-06-12 14:47:23 -0400

    finish desktop styling

--- src/components/HeroPage.tsx
+++ src/components/HeroPage.tsx
@@ -1,7 +1,7 @@
 const HeroPage = () => {
     return (
         <div className="rounded-md m-7 bg-slate-100 shadow-lg">
-            <div className="w-full h-full flex flex-col justify-start items-center rounded-md m-8 sm:items-start lg:flex-row lg:justify-center lg:gap-1 lg:mx-20 lg:my-5">
+            <div className="w-full h-full flex flex-col justify-start items-center rounded-md m-8 sm:items-start lg:flex-row lg:justify-center lg:gap-1 lg:my-5">
                 <div className="lg:flex lg:flex-col lg:justify-center lg:h-4/6">
                     <div className="mt-10 sm:mt-20 lg:w-4/6 lg:py-8">
                         <h1 className="font-semibold text-4xl sm:text-5xl lg:text-6xl mb-3">Well crafted abstract images</h1>
